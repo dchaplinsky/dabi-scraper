e06ce2677f3a15178128c5cf55ffdb2fd8244acc
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-01-15 11:48:03 +0200

    Registry spider refactored to also benefit from data pre-processing, new pagination parsing and smart caching

--- dabi/dabi/spiders/registry.py
+++ dabi/dabi/spiders/registry.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from scrapy import Spider, Item, Field, Selector, FormRequest
+from dabi.items import TakeFirstItemLoader
+import datetime
 
 
 class RegistryEntry(Item):
@@ -20,12 +22,18 @@ class RegistrySpider(Spider):
     allowed_domains = ["asdev.com.ua"]
 
     def start_requests(self):
+        now = datetime.datetime.now()
+
         for region in range(1, 28) + [99]:
             for year in range(2011, 2016):
                 for month in range(1, 13):
-                    yield self._build_request(region, year, month, page=1)
+                    invalidate = (year == now.year and month == now.month)
+                    yield self._build_request(
+                        region, year, month, page=1,
+                        invalidate_cache=invalidate
+                    )
 
-    def _build_request(self, region, year, month, page):
+    def _build_request(self, region, year, month, page, invalidate_cache):
         return FormRequest(
             url="http://asdev.com.ua/dabi/list.php?sort=num&order=DESC&page=%s" % page,
             formdata={
@@ -37,30 +45,39 @@ class RegistrySpider(Spider):
                 "year": year,
                 "month": month,
                 "page": page,
-                "region": region
+                "region": region,
+                "invalidate_cache": invalidate_cache
             },
             callback=self.parse)
 
     def parse(self, response):
         s = Selector(response)
         for tr in s.xpath("//table[contains(@class, 'listTable')]//tr[not(@class)][not(@id)]"):
-            item = RegistryEntry()
-            item["number"] = tr.xpath("./td[1]/text()").extract()
-            item["idabk"] = tr.xpath("./td[2]/text()").extract()
-            item["document"] = tr.xpath("./td[3]/text()").extract()
-            item["obj"] = tr.xpath("./td[4]/text()").extract()
-            item["cat"] = tr.xpath("./td[5]/text()").extract()
-            item["customer"] = tr.xpath("./td[6]/text()").extract()
-            item["tech_oversee"] = tr.xpath("./td[7]/text()").extract()
-            item["year"] = response.meta["year"]
-            item["month"] = response.meta["month"]
+            item = TakeFirstItemLoader(item=RegistryEntry(), selector=tr)
 
-            yield item
+            item.add_xpath("number", "./td[1]/text()")
+            item.add_xpath("idabk", "./td[2]/text()")
+            item.add_xpath("document", "./td[3]/text()")
+            item.add_xpath("obj", "./td[4]/text()")
+            item.add_xpath("cat", "./td[5]/text()")
+            item.add_xpath("customer", "./td[6]/text()")
+            item.add_xpath("tech_oversee", "./td[7]/text()")
 
-        for x in s.xpath("//div[@id='pages']/a/@href").re("page=(\d*)"):
-            yield self._build_request(
-                region=response.meta["region"],
-                year=response.meta["year"],
-                month=response.meta["month"],
-                page=x
-            )
+            item.add_value("year", unicode(response.meta["year"]))
+            item.add_value("month", unicode(response.meta["month"]))
+
+            yield item.load_item()
+
+        pages = s.xpath("//div[@id='pages']/a/@href").re("page=(\d*)")
+
+        if pages:
+            max_page = int(pages[-1])
+
+            for page in range(2, max_page + 1):
+                yield self._build_request(
+                    region=response.meta["region"],
+                    year=response.meta["year"],
+                    month=response.meta["month"],
+                    page=page,
+                    invalidate_cache=False
+                )
